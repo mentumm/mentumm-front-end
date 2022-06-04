cf2a06f582318b9e38029ed60275696c81942103
denvermullets <devmullets@gmail.com>
2022-06-03 20:20:19 -0400

    feat(coaches): set min width for cards, min height

--- src/components/Coach/Card.tsx
+++ src/components/Coach/Card.tsx
@@ -4,8 +4,10 @@ import * as React from "react";
 export const Card = (props: BoxProps) => (
   <Box
     bg={useColorModeValue("white", "gray.700")}
-    maxWidth="2xl"
+    maxWidth="xl"
+    minWidth="xl"
     mx="auto"
+    minHeight="2xs"
     p={{ base: "6", md: "8" }}
     rounded={{ sm: "lg" }}
     shadow={{ md: "base" }}
