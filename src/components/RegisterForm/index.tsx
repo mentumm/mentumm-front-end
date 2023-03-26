076c9c059fb396a1c293646476b31ab3a48c8f02
Jim Burch <49823725+jimburch@users.noreply.github.com>
2023-03-25 21:59:00 -0700

    switch to api client for user register

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -16,6 +16,7 @@ import {
 import { UserLoginProps } from "../../types";
 import axios from "axios";
 import { useCookies } from "react-cookie";
+import { menApiAuthClient } from "../../clients/mentumm";
 
 const NODE_API = process.env.REACT_APP_NODE_API;
 
@@ -89,13 +90,16 @@ const RegisterForm: React.FC<UserLoginProps> = (props) => {
     }
 
     try {
-      const createUser = await axios.post(`${NODE_API}/v1/user/register`, {
-        email: email,
-        password: password,
-        invite_code: inviteCode,
-        first_name: userFirstName,
-        last_name: userLastName,
-      });
+      const createUser = await menApiAuthClient().post(
+        `${NODE_API}/v1/user/register`,
+        {
+          email: email,
+          password: password,
+          invite_code: inviteCode,
+          first_name: userFirstName,
+          last_name: userLastName,
+        }
+      );
 
       if (!createUser) {
         throw Error("Unable to login");
