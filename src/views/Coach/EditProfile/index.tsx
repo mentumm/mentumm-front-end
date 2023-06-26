141b0bd1c8bb330a27fdc0e2818724682c8a6500
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-06-26 13:58:36 -0700

    fix(CoachingStyle): add missing 'kind' field for proper coach style tag updates (#131)

--- src/views/Coach/EditProfile/index.tsx
+++ src/views/Coach/EditProfile/index.tsx
@@ -75,7 +75,7 @@ export const EditProfile = ({ currentUser, setCurrentUser }) => {
         mixpanelEvent("Coach Edit Profile Viewed", {
           "Coach ID": coach.id,
           "Coach Name": `${coach.first_name} ${coach.last_name}`,
-          "Coach Styles": coachStyles.map((style) => style.name),
+          "Coach Styles": coach.styles.map((style) => style.name),
         });
       } catch (error) {
         console.log("Problem loading Coach Profile", error);
