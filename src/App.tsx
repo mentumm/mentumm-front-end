dd5cc69e49c51d3d20ef7b4dbfc369cf94fe45d4
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-07-25 18:46:58 -0700

    moving away from cookies for fetching user data

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import AppContainer from "./components/AppContainer";
 import Footer from "./components/Footer";
 import SignInWrapper from "./components/LoginWrapper";
 import NavBar from "./components/NavBar";
-import { CurrentUser } from "./types";
+import { CurrentUser, User } from "./types";
 import ActionPlan from "./views/ActionPlan";
 import { EditProfile } from "./views/Coach/EditProfile";
 import CoachBio from "./views/CoachBio";
@@ -23,6 +23,7 @@ import BookingConfirmation from "./views/BookingConfirmation";
 import { CoachExpertise } from "./views/CoachExpertise";
 import { ForgotPassword } from "./views/ForgotPassword";
 import { ResetPassword } from "./views/ResetPassword";
+import { menApiAuthClient } from "./clients/mentumm";
 
 const useStyles = createUseStyles((theme: DefaultTheme) => ({
   root: {
@@ -66,44 +67,61 @@ function App() {
   const [cookies] = useCookies(["growth_10_03142023"]);
 
   useEffect(() => {
-    if (cookies.growth_10_03142023) {
-      setCurrentUser({
-        id: cookies.growth_10_03142023.id,
-        last_sign_in: cookies.growth_10_03142023.last_sign_in,
+    // if (cookies.growth_10_03142023) {
+    //   setCurrentUser({
+    //     id: cookies.growth_10_03142023.id,
+    //     last_sign_in: cookies.growth_10_03142023.last_sign_in,
 
-        // leaving backward compatibility for now
-        first_name: cookies.growth_10_03142023.name
-          ? cookies.growth_10_03142023.name.split(" ")[0]
-          : cookies.growth_10_03142023.first_name,
-        last_name: cookies.growth_10_03142023.name
-          ? cookies.growth_10_03142023.name.split(" ")[1]
-          : cookies.growth_10_03142023.last_name,
-        //
+    //     // leaving backward compatibility for now
+    //     first_name: cookies.growth_10_03142023.name
+    //       ? cookies.growth_10_03142023.name.split(" ")[0]
+    //       : cookies.growth_10_03142023.first_name,
+    //     last_name: cookies.growth_10_03142023.name
+    //       ? cookies.growth_10_03142023.name.split(" ")[1]
+    //       : cookies.growth_10_03142023.last_name,
+    //     //
 
-        email: cookies.growth_10_03142023.email,
-        employer_id: cookies.growth_10_03142023.employer_id,
-        role: cookies.growth_10_03142023.role,
-        city: cookies.growth_10_03142023.city,
-        state: cookies.growth_10_03142023.state,
-        photo_url: cookies.growth_10_03142023.photo_url,
-        booking_url: cookies.growth_10_03142023.booking_url,
-        linkedin_url: cookies.growth_10_03142023.linkedin_url,
-        bio: cookies.growth_10_03142023.bio,
-        instagram_url: cookies.growth_10_03142023.instagram_url,
-        facebook_url: cookies.growth_10_03142023.facebook_url,
-        website_url: cookies.growth_10_03142023.website_url,
-        phone_number: cookies.growth_10_03142023.phone_number,
-        achievements1: cookies.growth_10_03142023.achievements1,
-        achievements2: cookies.growth_10_03142023.achievements2,
-        achievements3: cookies.growth_10_03142023.achievements3,
-        hobbies1: cookies.growth_10_03142023.hobbies1,
-        hobbies2: cookies.growth_10_03142023.hobbies2,
-        hobbies3: cookies.growth_10_03142023.hobbies3,
-        hobbies4: cookies.growth_10_03142023.hobbies4,
-        hobbies5: cookies.growth_10_03142023.hobbies5,
-        hobbies6: cookies.growth_10_03142023.hobbies6,
-      });
-    }
+    //     email: cookies.growth_10_03142023.email,
+    //     employer_id: cookies.growth_10_03142023.employer_id,
+    //     role: cookies.growth_10_03142023.role,
+    //     city: cookies.growth_10_03142023.city,
+    //     state: cookies.growth_10_03142023.state,
+    //     photo_url: cookies.growth_10_03142023.photo_url,
+    //     booking_url: cookies.growth_10_03142023.booking_url,
+    //     linkedin_url: cookies.growth_10_03142023.linkedin_url,
+    //     bio: cookies.growth_10_03142023.bio,
+    //     instagram_url: cookies.growth_10_03142023.instagram_url,
+    //     facebook_url: cookies.growth_10_03142023.facebook_url,
+    //     website_url: cookies.growth_10_03142023.website_url,
+    //     phone_number: cookies.growth_10_03142023.phone_number,
+    //     achievements1: cookies.growth_10_03142023.achievements1,
+    //     achievements2: cookies.growth_10_03142023.achievements2,
+    //     achievements3: cookies.growth_10_03142023.achievements3,
+    //     hobbies1: cookies.growth_10_03142023.hobbies1,
+    //     hobbies2: cookies.growth_10_03142023.hobbies2,
+    //     hobbies3: cookies.growth_10_03142023.hobbies3,
+    //     hobbies4: cookies.growth_10_03142023.hobbies4,
+    //     hobbies5: cookies.growth_10_03142023.hobbies5,
+    //     hobbies6: cookies.growth_10_03142023.hobbies6,
+    //   });
+    // }
+    const loadUser = async () => {
+      try {
+        const singleUser = await menApiAuthClient().get("/users", {
+          params: {
+            id: cookies.growth_10_03142023.id,
+          },
+        });
+
+        const user: User = singleUser.data[0];
+        console.log(user);
+
+      } catch (error) {
+        console.log("Problem loading Coach Profile", error);
+        throw new Error(error);
+      }
+    };
+    loadUser();
   }, [cookies]);
 
   return (
