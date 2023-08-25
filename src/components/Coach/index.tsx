7ca78b1482230b21058286b2cefade0108731c46
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-08-24 20:08:26 -0700

    updated Coach tile/card as per UI-v2 spec

--- src/components/Coach/index.tsx
+++ src/components/Coach/index.tsx
@@ -1,126 +1,122 @@
 import {
   Box,
-  HStack,
-  Icon,
-  Stack,
-  Tag,
-  TagLabel,
+  Flex,
   Text,
   Wrap,
+  Card,
+  CardBody,
+  CardFooter,
 } from "@chakra-ui/react";
 import React from "react";
-import { createUseStyles, DefaultTheme } from "react-jss";
 import { CoachProps, Tag as TagType } from "../../types";
-import { Card } from "./Card";
-import { CardContent } from "./CardContent";
-import { CardHeader } from "./CardHeader";
-import { UserAvatar } from "./UserAvatar";
-import { GoGlobe } from "react-icons/go";
+import { CoachName } from "./CoachName";
 import BookingInfo from "./BookingInfo";
 import { Link } from "react-router-dom";
-import { TagIcon } from "../TagIcon";
 import { CoachType } from "../../types";
-
-const useStyles = createUseStyles((theme: DefaultTheme) => ({
-  root: {
-    display: "flex",
-  },
-}));
 
 export const generateCoachUrl = (coach: CoachType) => {
   const name = `${coach.first_name} ${coach.last_name}`;
   return name.replace(/\W|_/g, "-").toLowerCase() + `-${coach.id}`;
 };
 
-const generateCoachTags = (tags: TagType[], slug: string) => {
-  let trimmedTags = [];
+const generateCoachTags = (tags: TagType[], slug: string, kind: string) => {
+  let trimmedTags: TagType[] = [];
 
   if (tags.length > 2) {
-    const remainingTags = tags.filter((tag: TagType) => tag.slug !== slug);
+    const remainingTags = tags.filter(tag => tag.slug !== slug);
 
-    // make sure to show the tag that the user searched for
     if (slug) {
-      trimmedTags.push(tags.find((tag) => tag.slug === slug));
-      remainingTags.slice(0, 2).map((tag) => trimmedTags.push(tag));
+      const matchingTag = tags.find(tag => tag.slug === slug);
+      if (matchingTag) {
+        trimmedTags.push(matchingTag);
+      }
+      trimmedTags = [...trimmedTags, ...remainingTags.slice(0, 2)];
     } else {
-      remainingTags.slice(0, 2).map((tag) => trimmedTags.push(tag));
+      trimmedTags = remainingTags.slice(0, 2);
     }
+
     // the extra +3 more tag
-    trimmedTags.push({
+    const moreTag: Partial<TagType> = {
       id: Math.floor(Math.random() * (2000 - 1000) + 1000),
-      name: `+ ${tags.length - 2} more`,
-    });
+      name: `+ ${tags.length - 2} more`
+    };
+
+    trimmedTags.push(moreTag as TagType);
   } else {
     trimmedTags = tags;
   }
-  return trimmedTags.map(
-    (tag) =>
-      !!tag && (
-        <Tag key={tag.id} backgroundColor={tag.color} size="lg">
-          <TagIcon icon={tag.icon} />
-          <TagLabel>{tag.name}</TagLabel>
-        </Tag>
-      )
-  );
+
+  return trimmedTags.map(tag => {
+    const tagName = kind === 'style' ? tag.name.toUpperCase() : tag.name;
+    const tagFontSize = kind === 'style' ? 'sm' : 'xs';
+
+    return (
+      <Text
+        fontSize={tagFontSize}
+        key={tag.id}
+        color="white"
+      >
+        {tagName}
+      </Text>
+    );
+  });
 };
 
+
 const Coach: React.FC<CoachProps> = (props) => {
-  const classes = useStyles();
   const { coachInfo, slug, booking, currentUser } = props;
-  const { first_name, last_name, expertise, city, state, photo_url } = coachInfo;
-  const getLocationText = (city?: string, state?: string) => {
-    if (city && state) {
-      return `${city}, ${state}`;
-    } else if (city && !state) {
-      return city;
-    } else if (!city && state) {
-      return state;
-    }
-    return null;
-  }
+  const { first_name, last_name, expertise, styles, photo_url } = coachInfo;
+
+  const coachImage = photo_url
+    ? photo_url
+    : "https://mentumm.com/wp-content/uploads/2022/06/mentumm_profile.png";
 
   return (
-    <Link to={`/coach/${generateCoachUrl(coachInfo)}`} className={classes.root}>
-      <Box as="section" py="6">
-        <Card
+    <Flex as={Link} to={`/coach/${generateCoachUrl(coachInfo)}`}>
+      <Card
+        bgImage={`linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(${coachImage})`}
+        backgroundSize="100%"
+        minWidth="23.5em"
+        maxWidth="23.5em"
+        rounded="sm"
+        shadow="base"
+        h="12.5em"
+        minH="12.5em"
+      >
+        <CardBody
+          h="10em"
+          minH="10em"
         >
-          <Stack
-            direction={{ base: "column", md: "row" }}
-            spacing={{ base: "4", md: "10" }}
-          >
-            <UserAvatar
-              name={`${first_name} ${last_name}`}
-              src={
-                photo_url
-                  ? photo_url
-                  : "https://mentumm.com/wp-content/uploads/2022/06/mentumm_profile.png"
-              }
-            />
-            <CardContent>
-              <CardHeader title={`${first_name} ${last_name}`} />
-              <Stack mt="1">
-                <HStack fontSize="md" mt={2}>
-                  <Icon as={GoGlobe} color="gray.500" />
-                  <Text>
-                    {getLocationText(city, state)}
-                  </Text>
-                </HStack>
-              </Stack>
-              <Wrap shouldWrapChildren mt={4}>
-                {expertise ? generateCoachTags(expertise, slug) : null}
-              </Wrap>
-            </CardContent>
-            {!!booking && (
-              <BookingInfo
-                booking={booking}
-                coach={coachInfo}
-                currentUser={currentUser}
-              />
-            )}
-          </Stack>
-        </Card>
-      </Box>
-    </Link>
+          <Box>
+            <CoachName title={`${first_name} ${last_name}`} />
+          </Box>
+          <Box>
+            {<Wrap shouldWrapChildren>
+              {styles && generateCoachTags(styles, slug, 'style')}
+            </Wrap>}
+          </Box>
+        </CardBody>
+        <CardFooter
+          bgColor="brand.400"
+          pb={0}
+          pt={1}
+          h="2.625em"
+          minH="2.625em"
+          rounded="sm"
+        >
+          <Wrap shouldWrapChildren mb={4}>
+            {expertise && generateCoachTags(expertise, slug, 'expertise')}
+          </Wrap>
+        </CardFooter>
+        {!!booking && (
+          <BookingInfo
+            booking={booking}
+            coach={coachInfo}
+            currentUser={currentUser}
+          />
+        )}
+      </Card>
+    </Flex>
   );
 };
 
