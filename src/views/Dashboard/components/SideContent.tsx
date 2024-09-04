f2a203482c7d347f939d83257e36e26e9636c54d
Ryan McCabe <ryan.mccabe089@gmail.com>
2024-09-03 18:28:40 -0700

    fix: resovled ts version issue

--- src/views/Dashboard/components/SideContent.tsx
+++ src/views/Dashboard/components/SideContent.tsx
@@ -1,10 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Box, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 const SideContent = () => {
   return (
-    <Box>
-      side content
+    <Box
+      w='336px'
+      bgColor='#061223'
+      zIndex='2'
+      borderRadius='1em'
+      my="2em"
+    >
+      <VStack>
+
+      </VStack>
     </Box>
   )
 }
